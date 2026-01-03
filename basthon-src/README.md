0713415e774319452bbec44b6b0fd90c658cc58c
_rweeda <renske.weeda@gmail.com>
2026-01-03 15:54:30 +0100

    playing around with AI

--- basthon-src/README.md
+++
@@ -1,5 +0,0 @@
-See [Basthon-Kernel's Readme](https://framagit.org/casatir/basthon-kernel/-/blob/master/README.md#basthon).
-
-
-    npm install
-    npm start
--- basthon-src/src/ts/declarations.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.png";
-
-declare module "jquery";
-declare module "jquery-ui";
-
-declare module "codemirror/mode/python/python";
-declare module "codemirror/mode/sql/sql";
-declare module "codemirror/mode/javascript/javascript";
-declare module "codemirror/mode/mllike/mllike";
